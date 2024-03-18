c8c730a85ea6ef74b2ce5fa855deb1840ae9b302
Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>
2024-03-18 12:02:58 -0700

    not using replication_queue

--- check_replication_queue.py
+++ check_replication_queue.py
@@ -12,22 +12,23 @@ logger = logging.getLogger("replication_queue")
 logger.setLevel(logging.INFO)
 
 
-def _is_replication_queue_empty(host: str, port: str):
+def _is_replication_queue_empty(host: str, port: str, allowed_delay: int):
     response = requests.post(
         url=f"http://{host}:{port}",
-        params={"query": "SELECT * FROM system.replication_queue"}
+        params={"query": "SELECT value FROM system.asynchronous_metrics where metric = 'ReplicasMaxAbsoluteDelay'"}
     )
-    return len(response.text.strip()) == 0
+    return response.text.strip() <= allowed_delay
 
 
 @click.command()
 @click.option("--host")
 @click.option("--port")
+@click.option("--delay")
 @click.option("--timeout", default=DEFAULT_TIMEOUT_THRESHOLD)
 @click.option("--poll", default=DEFAULT_POLL_PERIOD)
-def check_replication_queue(host: str, port: str, timeout: int, poll: int):
+def check_replication_queue(host: str, port: str, delay: int, timeout: int, poll: int):
     try:
-        waiting.wait(lambda: _is_replication_queue_empty(host, port), sleep_seconds=poll, timeout_seconds=timeout)
+        waiting.wait(lambda: _is_replication_queue_empty(host, port, delay), sleep_seconds=poll, timeout_seconds=timeout)
     except waiting.TimeoutExpired:
         logger.error("Replication queue still non-empty at the end of %s seconds" % timeout)
         sys.exit(1)

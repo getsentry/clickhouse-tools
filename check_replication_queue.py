470561d3706d3a0a724ba2ece2f43f4affa13afb
Rachel Chen <rachelchen@PL6VFX9HP4.attlocal.net>
2024-03-18 10:56:27 -0700

    use http client

--- check_replication_queue.py
+++ check_replication_queue.py
@@ -1,12 +1,9 @@
-from client import get_client
 import sys
 import waiting
 import click
 import logging
+import requests
 
-
-SNUBA_ADMIN_HOST = "127.0.0.1"
-CLICKHOUSE_CLIENT_PORT = "9000"
 DEFAULT_TIMEOUT_THRESHOLD = 300
 DEFAULT_POLL_PERIOD = 60
 
@@ -15,28 +12,28 @@ logger = logging.getLogger("replication_queue")
 logger.setLevel(logging.INFO)
 
 
-def _is_replication_queue_empty(client):
-    return len(client.execute("SELECT COUNT(*) FROM system.replication_queue LIMIT 1;")) == 0
+def _is_replication_queue_empty(host: str, port: str):
+    response = requests.post(
+        url=f"http://{host}:{port}",
+        params={"query": "SELECT * FROM system.replication_queue"}
+    )
+    return len(response.text.strip()) == 0
 
 
 @click.command()
-@click.option("--host", default=SNUBA_ADMIN_HOST)
+@click.option("--host")
+@click.option("--port")
 @click.option("--timeout", default=DEFAULT_TIMEOUT_THRESHOLD)
 @click.option("--poll", default=DEFAULT_POLL_PERIOD)
-def check_replication_queue(host: str, timeout: int, poll: int):
-    client = get_client(host, CLICKHOUSE_CLIENT_PORT)
+def check_replication_queue(host: str, port: str, timeout: int, poll: int):
     try:
-        waiting.wait(lambda: _is_replication_queue_empty(client), sleep_seconds=poll, timeout_seconds=timeout)
+        waiting.wait(lambda: _is_replication_queue_empty(host, port), sleep_seconds=poll, timeout_seconds=timeout)
     except waiting.TimeoutExpired:
-        client.disconnect()
         logger.error("Replication queue still non-empty at the end of %s seconds" % timeout)
         sys.exit(1)
     else:
-        client.disconnect()
         logger.info("Replication queue is empty")
         sys.exit(0)
-        
-
 
 if __name__ == '__main__':
     check_replication_queue()
